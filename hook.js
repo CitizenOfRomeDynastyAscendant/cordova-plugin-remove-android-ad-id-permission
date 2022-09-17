d2fbfafbaacfa3eef8362cb1af8da8c3bf69d7d6
CitizenOfRome <896147+CitizenOfRome@users.noreply.github.com>
2022-09-17 20:16:07 +0530

    Create hook.js

---
+++ hook.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const fs = require('fs');
+const xml2js = require('xml2js');
+
+module.exports = function (context) {
+  const parseString = xml2js.parseString;
+  const builder = new xml2js.Builder(); 
+  const manifestPath = context.opts.projectRoot + '/platforms/android/AndroidManifest.xml';
+  const androidManifest = fs.readFileSync(manifestPath).toString();
+  let manifestRoot;
+
+  if (androidManifest) {
+    parseString(androidManifest, (err, manifest) => {      
+      if (err) {
+        return console.error(err);
+      }
+      manifestRoot = manifest['manifest'];
+
+      if (!manifestRoot['uses-permission']) {
+        manifestRoot['uses-permission'] = [];
+      }
+
+      var permission = manifestRoot['uses-permission'].find(function(item) {
+        return item['$']['android:name'] === 'android.permission.WRITE_EXTERNAL_STORAGE';
+      });
+
+      if(permission) {
+        permission['$']['tools:node'] = 'replace';
+        fs.writeFileSync(manifestPath, builder.buildObject(manifest));
+      }
+    });
+  }
+};
